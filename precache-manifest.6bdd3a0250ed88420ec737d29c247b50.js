21340cdaa1c348ceff0169c00f3c398d509a99c2
Jim Liu <juliu@groupon.com>
2020-05-21 23:38:19 -0500

    Updates

---
+++ precache-manifest.6bdd3a0250ed88420ec737d29c247b50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eadf3e1e80b6d3319400b69dae55aaf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "878dfc4b5b24cb457f99",
+    "url": "/static/js/2.9602c6e8.chunk.js"
+  },
+  {
+    "revision": "c2496f987363ee1a26277a53c25e3675",
+    "url": "/static/js/2.9602c6e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9600c15adc80ab26f34",
+    "url": "/static/js/main.8b27a253.chunk.js"
+  },
+  {
+    "revision": "140175cd105ce8aaae93",
+    "url": "/static/js/runtime-main.1e798eb8.js"
+  }
+]);
