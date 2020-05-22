bdb4cb0fb29b3e059d1ba6c75f0ffb3c7eb95d7d
Jim Liu <juliu@groupon.com>
2020-05-22 00:27:52 -0500

    Updates

---
+++ precache-manifest.7e84cd129efbcc1709195c66256bfd63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a2a4ae5fb69477681ced8f2b0db46a",
+    "url": "/recoil-paint/index.html"
+  },
+  {
+    "revision": "7c424750cf8c8adcdfb8",
+    "url": "/recoil-paint/static/js/2.3dd31e08.chunk.js"
+  },
+  {
+    "revision": "3b8cfef4f53a23cddb753f8ae9f3c8ba",
+    "url": "/recoil-paint/static/js/2.3dd31e08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d8f949afb7be49519e4",
+    "url": "/recoil-paint/static/js/main.561049da.chunk.js"
+  },
+  {
+    "revision": "0a0689a8ce8eec00d685",
+    "url": "/recoil-paint/static/js/runtime-main.752b6890.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.643125f1f779bc5bb9c6bbbe52e25b99.js"
+  "/recoil-paint/precache-manifest.7e84cd129efbcc1709195c66256bfd63.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/recoil-paint/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
