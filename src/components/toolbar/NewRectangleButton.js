891a2fb319833f2836822db3b003e78161007b56
Jim Liu <juliu@groupon.com>
2020-05-24 18:27:45 -0500

    Refactor code with useRecoilCallback

--- src/components/toolbar/NewRectangleButton.js
+++ src/components/toolbar/NewRectangleButton.js
@@ -1,35 +1,58 @@
-import React from 'react';
-import { useRecoilState, useSetRecoilState, useRecoilValue } from 'recoil';
+import React, { useState } from 'react';
+import { useSetRecoilState, useRecoilValue, useRecoilCallback } from 'recoil';
 import Button from '../common/Button';
 import SquareIcon from '../icons/Square';
-import {
-  itemIdsState,
-  movingItemIdsState,
-  canvasLayoutState,
-  selectedIdsState,
-} from '../../recoil/atoms';
-import useMoveItems from '../hooks/useMoveItems';
+import { itemIdsState, canvasLayoutState, selectedIdsState, itemWithId } from '../../recoil/atoms';
+import useMove from '../hooks/useMove';
 import { createNewShape } from '../../recoil/defaults';
 
 export default function NewRectangleButton({ style }) {
-  const [itemIds, setItemIds] = useRecoilState(itemIdsState);
-  const [movingItemIds, setMovingItemIds] = useRecoilState(movingItemIdsState);
+  const [newItem, setNewItem] = useState(null);
+  const setItemIds = useSetRecoilState(itemIdsState);
   const setSelectedIds = useSetRecoilState(selectedIdsState);
   const canvasLayout = useRecoilValue(canvasLayoutState);
 
-  const { onMouseDown } = useMoveItems(({ status, origin }) => {
+
+  const createNewItem = useRecoilCallback(async ({ getPromise }, shapeParam) => {
+    let id = createNewShape(shapeParam);
+    const item = await getPromise(itemWithId(id));
+
+    return item;
+  });
+
+  const updatePosition = useRecoilCallback(({ set }, newValue) => {
+    set(itemWithId(newValue.id), newValue)
+  });
+
+  const { onMouseDown } = useMove(({ status, origin, offset }) => {
     if (status === 'start') {
-      let id = createNewShape({ x: origin.clientX - canvasLayout.x, y: origin.clientY - canvasLayout.y });
-      setItemIds([...itemIds, id]);
-      setSelectedIds([]);
-      setMovingItemIds([id]);
+      setNewItem(null);
+      createNewItem({ x: origin.clientX - canvasLayout.x, y: origin.clientY - canvasLayout.y })
+        .then(item => {
+          setNewItem(item);
+          setItemIds(items => [...items, item.id]);
+          setSelectedIds([]);
+        });
+    }
+
+    if (status === 'moving') {
+      if (newItem) {
+        updatePosition({
+          ...newItem,
+          x: origin.clientX + offset.x - canvasLayout.x,
+          y: origin.clientY + offset.y - canvasLayout.y
+        })
+      }
     }
 
     if (status === 'end') {
-      setSelectedIds([...movingItemIds]);
-      setMovingItemIds([]);
+      if (newItem) {
+        setNewItem(null);
+        setSelectedIds([newItem.id]);
+      }
     }
   })
+
 
 
   return <Button Icon={SquareIcon} style={style} onMouseDown={onMouseDown} />;
